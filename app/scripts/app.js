158279b368da7fa408460a8099ce41b7d0fd8f27
Tony Mehanny <amehanny@equinix.com>
2017-08-20 18:31:34 -0400

    Finished Assignment-6

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,32 @@
-
 (function() {
-    function config($stateProvider, $locationProvider) {
-      $locationProvider
-        .html5Mode({
-          enabled: true,
-          requireBase: false
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
     });
-
     $stateProvider
-       .state('landing', {
-          url: '/',
-          controller: 'LandingCtrl as landing',
-          templateUrl: '/templates/landing.html'
+      .state('landing', {
+        url: '/',
+        controller: 'LandingCtrl as landing',
+        templateUrl: '/templates/landing.html'
       })
-       .state('album', {
-           url: '/album',
-           controller: 'AlbumCtrl as albumControl',
-           templateUrl: '/templates/album.html'
+      .state('album', {
+        url: '/album',
+        controller: 'AlbumCtrl as albumControl',
+        templateUrl: '/templates/album.html'
       })
-        .state('collection', {
-           url: '/collection',
-           controller: 'CollectionCtrl as collection',
-           templateUrl: '/templates/collection.html'
+      .state('collection', {
+        url: '/collection',
+        controller: 'CollectionCtrl as collection',
+        templateUrl: '/templates/collection.html'
       });
-    }
 
-angular.module('blocJams', ['ui.router']);
+  }
 
-angular
-     .module('blocJams', ['ui.router'])
-     .config(config);
+  angular.module('blocJams', ['ui.router']);
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+
 })();
--- app/scripts/services/fixtures.js
+++ app/scripts/services/fixtures.js
@@ -1,46 +1,54 @@
-var albumPicasso = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: '1881',
-  albumArtUrl: 'assets/images/album_covers/07.png',
-  songs: [
-    { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-    { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-    { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-    { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
-    { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
-]
-};
+(function() {
+  function Fixtures() {
+    var Fixtures = {};
+    // Example Album
+    var albumPicasso = {
+      title: 'The Colors',
+      artist: 'Pablo Picasso',
+      label: 'Cubism',
+      year: '1881',
+      albumArtUrl: '/assets/images/album_covers/07.png',
+      songs: [
+        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+        { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+        { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+        { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+        { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+        ]
+      };
 
-// Another Example Album
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/09.png',
-    songs: [
+    // Another Example Album
+    var albumMarconi = {
+      title: 'The Telephone',
+      artist: 'Guglielmo Marconi',
+      label: 'EM',
+      year: '1909',
+      albumArtUrl: '/assets/images/album_covers/09.png',
+      songs: [
         { title: 'Hello, Operator?', duration: '1:01' },
         { title: 'Ring, ring, ring', duration: '5:01' },
         { title: 'Fits in your pocket', duration: '3:21'},
         { title: 'Can you hear me now?', duration: '3:14' },
         { title: 'Wrong phone number', duration: '2:15'}
-    ]
-};
+        ]
+      };
 
-// Assignment 11
-var albumDrake = {
-    title: 'More Life',
-    artist: 'Drake',
-    label: 'OVO',
-    year: '2017',
-    albumArtUrl: 'assets/images/album_covers/08.png',
-    songs: [
-        { title: 'Free Smoke', duration: '1:01' },
-        { title: 'No Long Talk', duration: '5:01' },
-        { title: 'Passionfruit', duration: '3:21'},
-        { title: 'Get It Together', duration: '3:14' },
-        { title: 'Blem', duration: '2:15'}
-    ]
-};
+    Fixtures.getAlbum = function() {
+      return albumPicasso;
+    };
+
+    Fixtures.getCollection = function(numberOfAlbums) {
+      this.objArray = [];
+      for (var i=0; i < numberOfAlbums; i++) {
+        this.objArray.push(angular.copy(albumPicasso));
+      }
+      return this.objArray;
+    };
+
+    return Fixtures;
+  }
+
+  angular
+  .module('blocJams')
+  .factory('Fixtures', Fixtures);
+})();

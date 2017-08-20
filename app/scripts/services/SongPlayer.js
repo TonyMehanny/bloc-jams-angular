75c8575ae2fc6b0dc057673c71e2efb4d65943c5
Tony Mehanny <amehanny@equinix.com>
2017-08-20 18:51:24 -0400

    Completed Assignment 7

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
   function SongPlayer() {
     var SongPlayer = {};
 
+    /**
+    * @desc Holds the currently playing song used for logic throughout services
+    * @type {Object}
+    */
     var currentSong = null;
 
     /**
@@ -29,17 +33,36 @@
       currentSong = song;
     };
 
+    /**
+    * @function playSong
+    * @desc Play current buzz object and sets playing property of song obj to true
+    * @param {Object} song
+    */
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    };
+
+    /**
+    * @function SongPlayer.play method
+    * @desc Plays song whether paused or not.
+    * @param {Object} song
+    */
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
 
       } else if (currentSong === song) {
-         if ( currentBuzzObject.isPaused() ) { currentBuzzObject.play(); }
+         if ( currentBuzzObject.isPaused() ) { playSong(song); }
       }
     };
 
+    /**
+    * @function SongPlayer.pause method
+    * @desc Pauses song whether and sets song.playing = false
+    * @param {Object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
